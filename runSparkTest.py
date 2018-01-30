4d8a4cb5dabec50d10e4ab01d5fa731cf8541de5
Ouro130Ros <NickMarshallx86@gmail.com>
2018-01-30 10:21:28 -0500

    SparkContext is hard

--- runSparkTest.py
+++ runSparkTest.py
@@ -2,9 +2,12 @@ import pyspark
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 
-NAMENODE_URI = 'hdfs://ec2-35-168-8-255.compute-1.amazonaws.com/'
+MASTER_URI = 'ec2-35-168-8-255.compute-1.amazonaws.com'
+NAMENODE_URI = 'hdfs://' + MASTER_URI + '/'
+SPARK_URI = 'spark://' + MASTER_URI + ":7077"
 
-sc = SparkSession.builder.appName("Playing With Spark").getOrCreate()
+conf = SparkConf().setAppName("SparkTests").setMaster(SPARK_URI)
+sc = SparkContext(conf = conf)
 
 hdfsQueryFile = sc.textFile(NAMENODE_URI + QUERY)
 print hdfsQueryFile.first()
