9a07a203e24bdbad17a7cee9880c8ce5ce756740
Ouro130Ros <NickMarshallx86@gmail.com>
2018-01-30 10:06:34 -0500

    Getting the context right

--- runSparkTest.py
+++ runSparkTest.py
@@ -4,8 +4,7 @@ from pyspark.sql import SparkSession
 
 NAMENODE_URI = 'hdfs://ec2-35-168-8-255.compute-1.amazonaws.com/'
 
-spark = SparkSession.builder.appName("Playing With Spark").getOrCreate()
-sc = SparkContext(conf = conf)
+sc = SparkSession.builder.appName("Playing With Spark").getOrCreate()
 
 hdfsQueryFile = sc.textFile(NAMENODE_URI + QUERY)
 print hdfsQueryFile.first()
